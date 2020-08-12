a04874765c59c02db84acf890266c4a9b959f88e
Roberto Diaz <roberto@theagilemonkeys.com>
2020-08-12 07:25:55 +0100

    avoid to override the index.js for the user project

--- boosterDockerContainer/start.sh
+++ boosterDockerContainer/start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-npm -g install express
 cp /data/appCode/boosterCode.zip  ./boosterCode.zip
 unzip ./boosterCode.zip
 rm -rf ./boosterCode.zip
-cp /data/appCode/index.js /app/dist/index.js
-node /app/dist/index.js
+node /app/node_modules/@boostercloud/framework-provider-kubernetes-infrastructure/dist/infrastructure/runtime
