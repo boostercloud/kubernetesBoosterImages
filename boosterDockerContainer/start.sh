b5c4c6d106430dc20c6383536ac923d9186c7975
Roberto Diaz <roberto@theagilemonkeys.com>
2021-03-17 16:50:49 +0000

    modified the runtime index path

--- boosterDockerContainer/start.sh
+++ boosterDockerContainer/start.sh
@@ -2,4 +2,4 @@
 cp /data/appCode/boosterCode.zip  ./boosterCode.zip
 unzip ./boosterCode.zip
 rm -rf ./boosterCode.zip
-node /app/node_modules/@boostercloud/framework-provider-kubernetes-infrastructure/dist/infrastructure/runtime
+node /app/node_modules/@boostercloud/framework-provider-kubernetes/dist/infrastructure/runtime
